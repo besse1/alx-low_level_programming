f4330ee098abd59d74369cae654e385652ce2dbc
besufikad <genibm.45@gmail.com>
2023-07-13 17:05:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *Descriptoin: main - prints string
  *
- * Return: Always 0 (Success)
+ * Return: 0 if Success
  */
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
