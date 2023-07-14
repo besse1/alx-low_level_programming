8eea52d2dba2f77a48f1656a6880e3e9dd8bdd57
besufikad <genibm.45@gmail.com>
2023-07-13 17:54:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
@@ -7,4 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-
+int main(void)
+{
+write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
