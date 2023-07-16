72beda0fef23d3670977565ff1bb2cd97a1c71d2
besufikad <genibm.45@gmail.com>
2023-07-16 13:07:18 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible combinations of two two-digit numbers.
- *
+(*
  * Return: 0 on success
  */
 int main(void)
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (i != 98 || i != 99)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
