697df1f565a0397be044d32a5f9c221053915a97
besufikad <genibm.45@gmail.com>
2023-07-15 23:00:15 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a=0;
+
+	while (a <= 10 )
+	{
+		printf("%d", a);
+		a += 1;
+	}
+	printf('\n');
+	return (0);
+}
+
