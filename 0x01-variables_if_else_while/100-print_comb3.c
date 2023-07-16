6deed0213046ed5c279f77bf5555b8cfa5136804
besufikad <genibm.45@gmail.com>
2023-07-16 05:01:18 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
+ * main - prints all possible different combinations of two digits.
  *
- *
- *
- *
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,4 +10,31 @@ int main(void)
 	int j = '0';
 
 	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+		
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8') && (j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j == '0';
+		j++;	
+		} 
+		j == '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
 
+}
