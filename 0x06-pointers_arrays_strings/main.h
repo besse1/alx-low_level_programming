ee17d7587608796e74b1699d45400b7e7ee47f1e
Besufikad Mengistu <genbim.45@gmail.com>
2023-07-28 18:22:54 +0300

    all

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * This header has all prototypes for the 0x06. C - More pointers, arrays and strings
- */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,10 +9,10 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *);
-
 char *rot13(char *);
+char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-#endif 
+
+#endif /*MAIN_H*/
