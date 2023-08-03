8f05eb1c7f3d384196e0f7e6e08f874d7fc3c6d4
besufikad <genibm.45@gmail.com>
2023-08-03 13:18:03 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,7 @@ int compare_string(char *s, int left, int right)
 	{
 		if (left == right || left == right + 1)
 			return (1);
-		return (0 + compare_string(s, left + 1, right + 1));
+		return (0 + compare_string(s, left + 1, right - 1));
 	}
 	return (0);
 }
@@ -39,5 +39,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (compare_string(s, 0, _strlen_recursion(s - 1)));
+	return (compare_string(s, 0, _strlen_recursion(s) - 1));
 }
