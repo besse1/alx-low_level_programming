40a1cc0d445fb929229078e942be8220011b4505
besufikad <genibm.45@gmail.com>
2023-08-14 14:44:54 -0700

    commit 1

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,9 +1,10 @@
-#ifndef _MAIN_H_
-#define _NAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* _MAIN_H_ */
+#endif /*MAIN_H*/
