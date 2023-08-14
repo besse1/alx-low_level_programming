25a1827d257ff5a830e377f563bb83f047503f72
besufikad <genibm.45@gmail.com>
2023-08-14 13:05:04 -0700

    commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program prints the name of the file it was compiled from.
+* Return: returns 0 (success)
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
