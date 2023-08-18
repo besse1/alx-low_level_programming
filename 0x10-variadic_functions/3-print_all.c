f4acf5522c236c17ad9536fee19145d59a8832f6
besufikad <genibm.45@gmail.com>
2023-08-18 13:09:52 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,9 +1,7 @@
 #include "variadic_functions.h"
-
 /**
-* print_all - a function that prints anything
-* @format: a list of types of arguments passed to the function
-* Return: returns nothing
+* print_all - function prints anything
+* @format: list of types of arguments passed to the function.
 */
 void print_all(const char * const format, ...)
 {
